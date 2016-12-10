2b2a1a025c6d8ed2c6e3190274a95126d54ec6c3
Matt <mattrthrelfall@gmail.com>
2016-12-10 17:04:08 +0000

    added getters and setters, really i want to see if git puts me on the graphs

--- core/src/com/pixeldot/ld37/WorldObjects/Block.java
+++ core/src/com/pixeldot/ld37/WorldObjects/Block.java
@@ -4,15 +4,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.pixeldot.ld37.Utilities.ContentManager;
-
-import static com.pixeldot.ld37.Game.HEIGHT;
 import static com.pixeldot.ld37.Game.PPM;
 
 public class Block {
-    Body body;
-    Texture texture;
-    ContentManager content;
+    private Body body;
+    private Texture texture;
 
     public Block(World w, Vector2 position, String textureKey){
         BodyDef bodydef = new BodyDef();
@@ -34,5 +30,21 @@ public class Block {
         batch.begin();
 
         batch.end();
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
     }
 }
