6b3535f713aba4ddfd7a343983170a0ca4c6ffe9
James Bulman <jamesbul2009@gmail.com>
2016-12-10 02:27:28 +0000

    Initial Commit

---
+++ android/src/com/pixeldot/ld37/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pixeldot.ld37;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pixeldot.ld37.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/pixeldot/ld37/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pixeldot.ld37.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pixeldot.ld37.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
